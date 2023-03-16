a462689e271ed40f0727a5f24c6d2a3ca312d77d
Florent <florent.hagard@gmail.com>
2023-03-16 22:51:26 +0100

    Documentation Project

--- project/internal/api/v1/fizzbuzz/stats.go
+++ project/internal/api/v1/fizzbuzz/stats.go
@@ -15,10 +15,8 @@ var mutex = &sync.Mutex{}
 // FizzBuzz endpoint. It retrieves the statistics from a shared global map that is updated by the
 // "FizzBuzzHits" function. The statistics include the FizzBuzz request parameters that were used
 // most frequently, as well as the number of times that those parameters were used.
-//
 // Parameters:
 // - c: a pointer to the fiber.Ctx object representing the HTTP request context.
-//
 // Returns:
 // - an error value, which is typically nil, since there is no meaningful error condition for this endpoint.
 // @Summary		Return FizzBuzz statistics.
